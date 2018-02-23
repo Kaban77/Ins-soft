ef52f4f7d55f111e35a9e7999d562be259a2d944
Maxim Demidov <demidovmax4@gmail.com>
2018-02-23 13:03:49 +0300

    Update LoginController.java

--- src/main/java/ru/demidov/insSoft/controllers/LoginController.java
+++ src/main/java/ru/demidov/insSoft/controllers/LoginController.java
@@ -1,32 +1,23 @@
 package ru.demidov.insSoft.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author Maxim Demiodov
  */
 @Controller
-@SessionAttributes("user")
 public class LoginController {
 
-	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
-
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = { "/", "/login" }, method = RequestMethod.GET)
-	public ModelAndView login(@RequestParam(value = "error", required = false) String error) {
+	public ModelAndView login(@RequestParam(value = "error", required = false) boolean error) {
 
 		ModelAndView model = new ModelAndView();
 
-		if (error != null) {
+		if (error != false) {
 			model.addObject("error", "Неверный логин или пароль!");
 		}
 		model.setViewName("login");
