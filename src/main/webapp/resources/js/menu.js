69ec4820d09640c138a8cd0342e6040ce8229746
Maxim Demidov <demidovmax4@gmail.com>
2018-03-03 12:29:34 +0300

    Update menu.js

--- src/main/webapp/resources/js/menu.js
+++ src/main/webapp/resources/js/menu.js
@@ -1,9 +1,4 @@
-function NewPolicy() {
-	//var params = "menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes"
-	//window.open("policy_page.html", "New Policy"/*, params*/)
-}
-
-function ClearForm() {
+function clearForm() {
 	document.getElementById("from_date").value = "";
 	document.getElementById("to_date").value = "";
 	document.getElementById("pol_num").value = "";
@@ -16,39 +11,6 @@ function checkKeyDown (elementId) {
 	
 	if(oldString.length === 2 || oldString.length ===5)
 		document.getElementById(elementId).value = oldString + ".";
-}
-
-function sendRequest(method, controller, data, callback) { //отправка запроса на сервер
-	const url = `/insSoft/${controller}`;//формируем URL
-	const request = new XMLHttpRequest();
-	request.contentType = "application/json";
-    	request.setRequestHeader("content-type", "application/text");
-    	request.setRequestHeader("cache-control", "no-cache");
-	
-	if(method === "GET") {
-		request.open(method, url + data, true);
-	}
-	else
-		request.open(method, url, true);
-	
-	if (data !== null && method !== "GET")
-        request.send(JSON.stringify(data));
-    else
-        request.send();
-		
-	request.onreadystatechange = () => {
-        if (request.readyState !== 4)
-            return;
-			
-		if (request.status >= 200 && request.status < 300) {
-				if (callback !== undefined && callback !== null) {
-					const result = JSON.parse(request.responseText);
-					callback(result);
-				}
-		} else {
-				console.log(`${request.status}: ${request.statusText}`);
-		}
-	};
 }
 
 function findPolicy() {  //Поиск результата по заданым данным
@@ -84,9 +46,10 @@ function showResult(result) {
 		tr.insertCell(4).innerHTML = "Страхователь";
 		for(i = 0; i < result.length; i++) {
 			tr = table.insertRow(i + 1);
+			
 			tr.insertCell(0).innerHTML = i + 1;
 			tr.insertCell(1).innerHTML = result[i].policyNumber;
-			tr.insertCell(2).innerHTML = result[i].beginDate + " - " + result[i].endDate;
+			tr.insertCell(2).innerHTML = dateParser(result[i].beginDate) + " - " + dateParser(result[i].endDate);
 			tr.insertCell(3).innerHTML = result[i].car;
 			tr.insertCell(4).innerHTML = result[i].insurantName;
 		}
