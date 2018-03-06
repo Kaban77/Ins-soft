8f508bafb06f2eea78fc1917d432df353e89925b
Maxim Demidov <demidovmax4@gmail.com>
2018-03-06 19:32:14 +0300

    Create Insurant.java

---
+++ src/main/java/ru/demidov/insSoft/objects/Insurant.java
@@ -0,0 +1,78 @@
+package ru.demidov.insSoft.objects;
+
+public class Insurant {
+	private int id;
+	private String surname;
+	private String name;
+	private String patronymic;
+	private String birthDate;
+	private String sex;
+	private Document document;
+
+	public Insurant() {
+	}
+
+	public Insurant(String surname, String name, String patronymic, String birthDate) {
+		this.surname = surname;
+		this.name = name;
+		this.patronymic = patronymic;
+		this.birthDate = birthDate;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPatronymic() {
+		return patronymic;
+	}
+
+	public void setPatronymic(String patronymic) {
+		this.patronymic = patronymic;
+	}
+
+	public String getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public Document getDocument() {
+		return document;
+	}
+
+	public void setDocument(Document document) {
+		this.document = document;
+	}
+
+}
