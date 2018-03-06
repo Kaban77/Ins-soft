fed3b4279a7bc307005db4497b9f577423bb7f28
Maxim Demidov <demidovmax4@gmail.com>
2018-03-06 19:31:52 +0300

    Create Document.java

---
+++ src/main/java/ru/demidov/insSoft/objects/Document.java
@@ -0,0 +1,51 @@
+package ru.demidov.insSoft.objects;
+
+public class Document {
+
+	private int id;
+	private int docType;
+	private String serial;
+	private String number;
+	private String dateOfIssue;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getDocType() {
+		return docType;
+	}
+
+	public void setDocType(int docType) {
+		this.docType = docType;
+	}
+
+	public String getSerial() {
+		return serial;
+	}
+
+	public void setSerial(String serial) {
+		this.serial = serial;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getDateOfIssue() {
+		return dateOfIssue;
+	}
+
+	public void setDateOfIssue(String dateOfIssue) {
+		this.dateOfIssue = dateOfIssue;
+	}
+
+}
