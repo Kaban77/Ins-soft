9be86d496777e861fcab290fd49073161405b5ac
Maxim Demidov <demidovmax4@gmail.com>
2018-03-10 10:29:05 +0300

    Create CarDaoImpl.java

---
+++ src/main/java/ru/demidov/insSoft/db/CarDaoImpl.java
@@ -0,0 +1,46 @@
+package ru.demidov.insSoft.db;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.ColumnMapRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import ru.demidov.insSoft.interfaces.CarDao;
+
+@Component
+public class CarDaoImpl implements CarDao {
+
+	@Autowired
+	private JdbcTemplate jdbctemplate;
+
+	private static final String getBrands = "select brand_id, brand_name from policies.t_brand_directory where upper(brand_name) like :brandName";
+	private static final String getModels = "select model_id, model_name from policies.t_car_directory where brand_id = :brandId";
+
+	private static final Logger logger = LoggerFactory.getLogger(CarDaoImpl.class);
+
+	@Override
+	public List<Map<String, Object>> findBrands(String brandName) {
+		try {
+			return jdbctemplate.query(getBrands, new Object[] { "%" + brandName.toUpperCase().trim() + "%" }, new ColumnMapRowMapper());
+		} catch (Exception e) {
+			logger.info(" findBrands " + e.getMessage());
+			return new ArrayList<Map<String, Object>>();
+		}
+	}
+
+	@Override
+	public List<Map<String, Object>> findModels(Integer brandId) {
+		try {
+			return jdbctemplate.query(getModels, new Object[] { brandId }, new ColumnMapRowMapper());
+		} catch (Exception e) {
+			logger.info(" findModels " + e.getMessage());
+			return new ArrayList<Map<String, Object>>();
+		}
+	}
+}
