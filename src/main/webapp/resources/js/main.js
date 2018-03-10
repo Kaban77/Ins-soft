c9f6215e4c3afa79e0a85d1c33ba54dde784b5c2
Maxim Demidov <demidovmax4@gmail.com>
2018-03-10 10:48:54 +0300

    Update main.js

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -4,7 +4,6 @@ function sendRequest(method, controller, data, callback) { //отправка з
 	
 	if(method === "GET") {
 		request.open(method, url + data, true);
-		request.setRequestHeader("content-type", "application/text");
 	}
 	else {
 		request.open(method, url, true);
@@ -13,9 +12,8 @@ function sendRequest(method, controller, data, callback) { //отправка з
 		const token = document.getElementById("token").value;
 		
 		request.setRequestHeader(header, token);
-		request.setRequestHeader("content-type", "application/json");
 	}
-	
+	request.setRequestHeader("content-type", "application/json");
 	request.contentType = "application/json";
     request.setRequestHeader("cache-control", "no-cache");
     
@@ -29,31 +27,30 @@ function sendRequest(method, controller, data, callback) { //отправка з
             return;
 			
 		if (request.status >= 200 && request.status < 300) {
-				if (callback !== undefined && callback !== null) {
-					try {
-						const result = JSON.parse(request.responseText);
-						callback(result);
-					}
-					catch(e) {
-						callback();
-					}
+			if (callback !== undefined && callback !== null) {
+				try {
+					const result = JSON.parse(request.responseText);
+					callback(result);
 				}
+				catch(e) {
+					callback();
+				}
+			}
 		} else {
 			console.log(`${request.status}: ${request.statusText}`);
 		}
 	};
 }
 
-function dateParser(date) {
-	var temp = new Date(date);
+function parseDate(date) {
+	let temp = new Date(date);
 	return temp.toLocaleDateString();
 }
 
-function tryParseDate(date) {
-	
-	var day = date.substring(0, 2);
-	var month = date.substring(3, 5);
-	var year = date.substring(6, 10);
+function checkIsValidDate(date) {
+	let day = date.substring(0, 2);
+	let month = date.substring(3, 5);
+	let year = date.substring(6, 10);
 	
 	if(isNaN(Date.parse(month + " " + day + " " + year)))
 		return false;
