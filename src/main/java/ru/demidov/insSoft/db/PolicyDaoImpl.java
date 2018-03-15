8b9fb406d5f095a63b1ceb64302382bb2d1b246d
Maxim Demidov <demidovmax4@gmail.com>
2018-03-15 10:18:48 +0300

    Change Policy class

--- src/main/java/ru/demidov/insSoft/db/PolicyDaoImpl.java
+++ src/main/java/ru/demidov/insSoft/db/PolicyDaoImpl.java
@@ -44,12 +44,12 @@ public class PolicyDaoImpl implements PolicyDao {
 
 	@Override
 	@Transactional(readOnly = true)
-	public PolicyFromDB findPoliciesById(Integer policyId) {
+	public Policy findPoliciesById(Integer policyId) {
 		try {
-			return jdbctemplate.queryForObject(getPolicyById, new Object[] { policyId }, new PolicyFromDBRowMapper());
+			return jdbctemplate.queryForObject(getPolicyById, new Object[] { policyId }, new PolicyRowMapper());
 		} catch (Exception e) {
 			logger.info(e.getMessage());
-			return new PolicyFromDB();
+			return new Policy();
 		}
 	}
 
@@ -57,7 +57,7 @@ public class PolicyDaoImpl implements PolicyDao {
 	@Transactional(readOnly = true)
 	public List<Policy> findPoliciesByParam(PolicyForSearch policy) {
 
-		String sql = "select * from policies.v_policy_info where upper(policy_number) like :policyNumber";
+		String sql = "select * from policies.v_policy_full where upper(policy_number) like :policyNumber";
 		ArrayList<String> args = new ArrayList<String>();
 
 		args.add("%" + policy.getPolicyNumber().toUpperCase().trim() + "%");
@@ -81,51 +81,54 @@ public class PolicyDaoImpl implements PolicyDao {
 
 	@Override
 	@Transactional(propagation = Propagation.REQUIRED)
-	public Coefficients insertPolicy(PolicyToDB policy) {
+	public Policy insertPolicy(Policy policy) {
 		try {
 			int policyId = jdbctemplate.queryForObject(getPolicyId, Integer.class);
 			policy.setPolicyId(policyId);
 
 			Coefficients coeff = new CoeffCalculator(jdbctemplate).calcPremium(policy);
 
-			jdbctemplate.update(insertPolicy, new Object[] { policyId, "EEE" + policyId, policy.getInsurantId(), coeff.getPremium() });
-			jdbctemplate.update(insertCar, new Object[] { policyId, policy.getModelId(), policy.getYearOfIssue(), policy.getVin(), policy.getRegisterSign(), policy.getEnginePower() });
+			jdbctemplate.update(insertPolicy, new Object[] { policyId, "EEE" + policyId, policy.getInsurant().getId(), coeff.getPremium() });
+			jdbctemplate.update(insertCar, new Object[] { policyId, policy.getModelId(), policy.getYearOfIssueCar(), policy.getVin(), policy.getRegisterSign(), policy.getEnginePower() });
 			jdbctemplate.update(insertCoeff, new Object[] { policyId, coeff.getTariff(), coeff.getBonus(), coeff.getPower(), coeff.getSeason(), coeff.getAgeAndExperience(), coeff.getPeriod(),
 					coeff.getDriverLimit(), coeff.getTerritory() });
 
-			return coeff;
+			policy.setCoeff(coeff);
+			return policy;
 
 		} catch (Exception e) {
 			logger.info(" insertPolicy " + e.getMessage());
-			return new Coefficients();
+			return new Policy();
 		}
 	}
 
 	@Override
 	@Transactional
-	public Coefficients updatePolicy(PolicyToDB policy) {
+	public Policy updatePolicy(Policy policy) {
 		try {
-			if (policy.getInsurantId() != null) {
+			if (policy.getInsurant().getId() != null) {
 				String updateInsurant = "update policies.t_policy set insurant_id = :insurantId where policy_id = :policyId";
-				jdbctemplate.update(updateInsurant, new Object[] { policy.getInsurantId(), policy.getPolicyId() });
+				jdbctemplate.update(updateInsurant, new Object[] { policy.getInsurant().getId(), policy.getPolicyId() });
 			} else {
 				String selectInsurantId = "select insurant_id from policies.t_policy where policy_id = :policyId";
 				int insurantId = jdbctemplate.queryForObject(selectInsurantId, Integer.class, new Object[] { policy.getPolicyId() });
 
-				policy.setInsurantId(insurantId);
+				Insurant insurant = new Insurant();
+				insurant.setId(insurantId);
+				policy.setInsurant(insurant);
 			}
 			jdbctemplate.update(updateCar, new Object[] { policy.getModelId(), policy.getYearOfIssue(), policy.getVin(), policy.getRegisterSign(), policy.getEnginePower(), policy.getPolicyId() });
 
 			Coefficients coeff = new CoeffCalculator(jdbctemplate).calcPremium(policy);
 
 			jdbctemplate.update(updateCoeff, new Object[] { coeff.getTariff(), coeff.getBonus(), coeff.getPower(), coeff.getSeason(), coeff.getAgeAndExperience(), coeff.getPeriod(),
-					coeff.getDriverLimit(), coeff.getTerritory(), coeff.getPolicyId() });
-			jdbctemplate.update(updatePremium, new Object[] { coeff.getPremium(), coeff.getPolicyId() });
+					coeff.getDriverLimit(), coeff.getTerritory(), policy.getPolicyId() });
+			jdbctemplate.update(updatePremium, new Object[] { coeff.getPremium(), policy.getPolicyId() });
 
 			return coeff;
 		} catch (Exception e) {
 			logger.info(e.getMessage());
-			return new Coefficients();
+			return new Policy();
 		}
 	}
 
@@ -159,58 +162,72 @@ public class PolicyDaoImpl implements PolicyDao {
 
 		@Override
 		public Policy mapRow(ResultSet rs, int rowNum) throws SQLException {
+			Document document = rowDocument(rs);
+			Insurant insurant = rowInsurant(rs, document);
+			Coefficients coeff = rowCoefficients(rs);
+
 			Policy policy = new Policy();
 
-			policy.setBeginDate(rs.getDate("begin_date").toString());
-			policy.setEndDate(rs.getDate("end_date").toString());
+			policy.setInsurant(insurant);
+			policy.setCoeff(coeff);
 			policy.setPolicyId(rs.getInt("policy_id"));
 			policy.setPolicyNumber(rs.getString("policy_number"));
-			policy.setPolicyState(rs.getInt("state"));
-			policy.setModelId(rs.getInt("model_id"));
-			policy.setCar(rs.getString("car"));
-			policy.setInsurantName(rs.getString("insurant_name"));
-			policy.setInsurantId(rs.getInt("insurant_id"));
-
-			return policy;
-		}
-	}
-
-	private static final class PolicyFromDBRowMapper implements RowMapper<PolicyFromDB> {
-
-		@Override
-		public PolicyFromDB mapRow(ResultSet rs, int rowNum) throws SQLException {
-			PolicyFromDB policy = new PolicyFromDB();
-			Coefficients coeff = new Coefficients();
-
-			coeff.setAgeAndExperience(rs.getDouble("age_and_experience"));
-			coeff.setBonus(rs.getDouble("bonus"));
-			coeff.setDriverLimit(rs.getDouble("driver_limit"));
-			coeff.setPeriod(rs.getDouble("period"));
-			coeff.setPolicyId(rs.getInt("policy_id"));
-			coeff.setPower(rs.getDouble("power"));
-			coeff.setSeason(rs.getDouble("season"));
-			coeff.setTariff(rs.getInt("tariff"));
-			coeff.setTerritory(rs.getDouble("territory"));
-			coeff.setPremium();
-
-			policy.setCoeff(coeff);
-			policy.setPolicyId(coeff.getPolicyId());
+			policy.setPolicyState(rs.getString("state_name"));
+			policy.setPolicyStateId(rs.getInt("state_id"));
+			policy.setBeginDate(rs.getDate("begin_date").toString());
+			policy.setEndDate(rs.getDate("end_date").toString());
+			policy.setInsutantFullName(rs.getString("insurant_full_name"));
 			policy.setBrandId(rs.getInt("brand_id"));
 			policy.setBrandName(rs.getString("brand_name"));
-			policy.setEnginePower(rs.getInt("engine_power"));
-			policy.setInsurantId(rs.getInt("insurant_id"));
-			policy.setInsutantFullName(rs.getString("full_name"));
-			policy.setLicenseNumber(rs.getString("doc_number"));
-			policy.setLicenseSerial(rs.getString("doc_serial"));
 			policy.setModelId(rs.getInt("model_id"));
 			policy.setModelName(rs.getString("model_name"));
-			policy.setPolicyState(rs.getInt("state"));
+			policy.setEnginePower(rs.getInt("engine_power"));
 			policy.setRegisterSign(rs.getString("register_sign"));
 			policy.setVin(rs.getString("vin"));
 			policy.setYearOfIssueCar(rs.getInt("year_of_issue"));
 
 			return policy;
 		}
-	}
 
+		private Document rowDocument(ResultSet rs) throws SQLException {
+			Document document = new Document();
+
+			document.setDateOfIssue(rs.getDate("date_of_issue_doc").toString());
+			document.setId(rs.getInt("doc_id"));
+			document.setNumber(rs.getString("doc_number"));
+			document.setSerial(rs.getString("doc_serial"));
+
+			return document;
+		}
+
+		private Insurant rowInsurant(ResultSet rs, Document document) throws SQLException {
+			Insurant insurant = new Insurant();
+
+			insurant.setDocument(document);
+			insurant.setBirthDate(rs.getDate("birth_date").toString());
+			insurant.setId(rs.getInt("insurant_id"));
+			insurant.setName(rs.getString("name"));
+			insurant.setPatronymic(rs.getString("patronymic"));
+			insurant.setSurname(rs.getString("surname"));
+			insurant.setSex(rs.getString("sex"));
+
+			return insurant;
+		}
+
+		private Coefficients rowCoefficients(ResultSet rs) throws SQLException {
+			Coefficients coeff = new Coefficients();
+
+			coeff.setAgeAndExperience(rs.getDouble("age_and_experience"));
+			coeff.setBonus(rs.getDouble("bonus"));
+			coeff.setDriverLimit(rs.getDouble("driver_limit"));
+			coeff.setPeriod(rs.getDouble("period"));
+			coeff.setPower(rs.getDouble("power"));
+			coeff.setSeason(rs.getDouble("season"));
+			coeff.setTariff(rs.getInt("tariff"));
+			coeff.setTerritory(rs.getDouble("territory"));
+			coeff.setPremium();
+
+			return coeff;
+		}
+	}
 }
